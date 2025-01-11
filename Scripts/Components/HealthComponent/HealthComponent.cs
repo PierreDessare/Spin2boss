5c475e6beefce58ef484dbebd3721f82cdd84e90
Sarah-Keppler <sarah.keppler@epitech.eu>
2025-01-12 00:18:04 +0100

    feat(death system): Implement death system

--- Scripts/Components/HealthComponent/HealthComponent.cs
+++ Scripts/Components/HealthComponent/HealthComponent.cs
@@ -3,21 +3,34 @@ using Godot;
 
 public partial class HealthComponent : Node
 {
-	[Signal]
-	public delegate void HealthChangedEventHandler(int newValue);
+	[Signal] public delegate void HealthChangedEventHandler(int newValue);
+	[Signal] public delegate void DeathStatusChangedEventHandler(bool isDead);
 
-	[Export]
-	public int maxHealth = 100;
-	
-	[Export]
-	private int _health = 0;
+	[Export] public int maxHealth = 100;	
+	[Export] private int _health = 0;
 	public int Health
 	{
 		get => _health;
-		set 
+		private set
 		{
 			_health = Math.Clamp(value, 0, maxHealth);
+			if (0 >= Health) {
+				IsDead = true;
+			}
+			if (0 < Health && IsDead) {
+				IsDead = false;
+			}
 			EmitSignal(SignalName.HealthChanged, _health);
+		}
+	}
+	[Export] private bool _isDead = true;
+	public bool IsDead
+	{
+		get => _isDead;
+		private set
+		{
+			_isDead = value;
+			EmitSignal(SignalName.DeathStatusChanged, _isDead);
 		}
 	}
 
@@ -25,17 +38,23 @@ public partial class HealthComponent : Node
 	{
 		Health -= value;
 	}
-
 	public void Heal(int value)
 	{
 		Health += value;
+	}
+	public void Kill()
+	{
+		Health = 0;
 	}
 
 	public override void _UnhandledInput(InputEvent @event)
 	{
 		if (@event.IsActionPressed("DebugKey")) {
-			GD.Print("[DEBUG KEY]: Healing player of 10 HP");
+			GD.Print("[DEBUG KEY]: Healing of 10 HP");
 			Heal(10);
+		} else if (@event.IsActionPressed("SecondDebugKey")) {
+			GD.Print("[SECOND DEBUG KEY]: Killing");
+			Kill();
 		}
 	}
 }
